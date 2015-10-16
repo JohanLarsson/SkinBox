9d7a118b70ce372454a46e6a8c4156e1800c3edd
Johan Larsson <johan_gavle1@hotmail.com>
2015-10-16 18:59:41 +0200

    Initial

---
+++ ResourceKeyBox/Keys.cs
@@ -0,0 +1,24 @@
+﻿namespace ResourceKeyBox
+{
+    using System.Collections.Generic;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+
+    public static class Keys
+    {
+        private static readonly Dictionary<string, ComponentResourceKey> Cache = new Dictionary<string, ComponentResourceKey>();
+
+        public static ResourceKey BlueBrushKey => Get();
+
+        private static ComponentResourceKey Get([CallerMemberName] string caller = null)
+        {
+            ComponentResourceKey key;
+            if (!Cache.TryGetValue(caller, out key))
+            {
+                key = new ComponentResourceKey(typeof(Keys), caller);
+                Cache.Add(caller, key);
+            }
+            return key;
+        }
+    }
+}
---
+++ ResourceKeyBox/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace ResourceKeyBox
+{
+    using System.Windows;
+    using System.Windows.Media;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Resources[Keys.BlueBrushKey] = Brushes.Honeydew;
+        }
+    }
+}
